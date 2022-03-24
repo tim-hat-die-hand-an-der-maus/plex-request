e5bc0af510ccb067623cb0138e32f2dae05b63a1
Torben Carstens <git@carstens.tech>
2022-03-24 21:32:42 +0100

    Forward plex-resolver /movies

--- src/Controller/PlexController.php
+++ src/Controller/PlexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpClient\NativeHttpClient;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,10 +11,13 @@ class PlexController
     #[Route('/plex')]
     public function available_movies(): Response
     {
-        $available = ["12 Monkeys"];
+        $client = new NativeHttpClient();
+        $response = $client->request("GET", "http://127.0.0.1:8080/movies");
+        $content = $response->getContent();
 
-        return new Response(
-            '<html lang="en"><body>' . implode('<br>', $available)  . '</body></html>'
-        );
+        $response = new Response($content);
+        $response->headers->set("Content-Type", "application/json");
+
+        return $response;
     }
 }
