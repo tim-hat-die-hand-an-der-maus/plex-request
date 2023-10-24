ca9361df11d05c44e64a7a06594a627e530f05cb
Torben Carstens <git@carstens.tech>
2023-10-24 16:17:55 +0200

    Add script to run/reset db

---
+++ run_db.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+sudo podman stop plexrequest-pg
+sudo podman rm plexrequest-pg
+sudo podman run -d --name plexrequest-pg -e POSTGRES_USER=use -e POSTGRES_USER=user -e POSTGRES_PASSWORD=password -p 5432:5432 postgres
+sleep 10
+sudo podman exec -it plexrequest-pg psql -U user -c 'CREATE DATABASE bwm;'
