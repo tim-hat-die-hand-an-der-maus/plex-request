5788565e164d890a5580b13754e0b5ce0be28002
Torben Carstens <git@carstens.tech>
2023-10-25 16:55:44 +0200

    Fix api-token compare

--- lib/plex_request_web/router.ex
+++ lib/plex_request_web/router.ex
@@ -34,7 +34,7 @@ defmodule PlexRequestWeb.Router do
              |> put_resp_header("Content-Type", "application/json")
              |> resp(401, '{"message": "Missing `X-Api-Token` header`"}')
              |> halt()
-      cmptoken -> case Plug.Crypto.secure_compare(correct_token, cmptoken) do
+      [cmptoken] -> case Plug.Crypto.secure_compare(correct_token, cmptoken) do
                   true -> conn
                   false -> conn
                            |> put_resp_header("Content-Type", "application/json")
